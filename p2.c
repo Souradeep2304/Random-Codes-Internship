2b161c691570b69dbce53a2134c9dda650fb5cb3
root <root@localhost.localdomain>
2019-06-08 01:17:55 -0400

    variable names updated

--- p2.c
+++ p2.c
@@ -5,47 +5,46 @@
 
 void main()
 {
-int n2=0;
-char new[]="Hello maam my name is oso. amma";
+int number_of_palindromes=0;
+char paragraph[]="I maam my name is oso. amma %  121  I"; 
 //gets(new);
-int l=strlen(new);
 //char sep[]=" ";
-char sep[]=",. ";
-char *w=strtok(new,sep);
-while(w!=NULL)
+char sep[]=",. ";			//seperator
+char *tokens=strtok(paragraph,sep);
+while(tokens!=NULL)
 
 					//checking palindrome
 {
 int j;
-int k=0,l,f=0;
-char t[100];
-char p[100];
-strcpy(p,w);
-l=strlen(p);
+int k=0,len,f=0;			//k is the size of temporary character array to store reversed word, l is to store length, f is flag
+char temp[100];				//to store reversed word
+char org[100];				//to store original word
+strcpy(org,tokens);
+len=strlen(org);
+
 					//reversing the string
-for ( j = l - 1; j >= 0; j--)
+for ( j = len - 1; j >= 0; j--)
   	{
 		
-  		t[k] = p[j];
+  		temp[k] = org[j];
 		k++;
 		
   	}
-t[k]='\0';				//mark the end of the string 
+temp[k]='\0';				//mark the end of the string 
 					//comparing the reversed and actual string
-for(int o=0;o<l;o++)
+for(int o=0;o<len;o++)
 {
-if(t[o]==p[o])
+if(temp[o]==org[o])                     //tolower() to ignore case
 {f=0;}
 else
 f=1;
 }
 if(f==0)
 {
-printf("%s is palindrome.\n",p);
-n2++; 					//counting the number of palindromes
+printf("%s is palindrome.\n",org);
+number_of_palindromes++; 		//counting the number of palindromes
 }
-//w=strtok(NULL,sep);
-w=strtok(NULL,sep);
+tokens=strtok(NULL,sep);
 }
-printf("Number of Palindromes:%d\n",n2);
+printf("Number of Palindromes:%d\n",number_of_palindromes);
 }
